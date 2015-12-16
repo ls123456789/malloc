2acd19dab4b3f8d272110d0b92dd620d754a5c70
ls123456789 <2953757467@qq.com>
2015-12-16 18:45:09 +0800

    malloc
    先定义两个int型的指针，malloc动态存储分配两个（16*4）个字节的int型的空间，分别赋给两个指针，再用四个for循环分别分配的32个i
    nt型空间赋值和输出，再输出两个指针所指向的空间的首地址，有时会发现分配的两个是连续的。

--- malloc/main.cpp
+++ malloc/main.cpp
@@ -10,6 +10,18 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int *p,*q;
+    p = (int *)malloc(16*4);
+    q = (int *)malloc(16*4);
+    for (int i = 0; i<16; i++)
+        p[i] = i*10;
+    for (int j = 0; j<16; j++)
+        q[j] = j*100;
+    
+    for (int j = 0; j<16; j++)
+        printf("p[%d] = %d \n",j,p[j]);
+    for (int j = 0; j<16; j++)
+        printf("q[%d] = %d \n",j,q[j]);
+    printf("%x  %x\n",p,q);
     return 0;
 }
